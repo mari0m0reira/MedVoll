53cffce6b4e377bcc0f4078dc7007fe01fe75ba3
mari0m0reira <mariomsjunior@gmail.com>
2024-01-15 23:39:05 -0300

    Adicionando método GET

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,12 +2,18 @@ package med.voll.api.controller;
 
 import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -21,4 +27,16 @@ public class MedicoController {
 		repository.save(new Medico(dados));
 
 	}
+
+	@GetMapping
+	public Page<DadosListagemMedico> listar(@PageableDefault(size = 10 , sort = {"nome"}) Pageable paginacao){
+		return repository.findAll(paginacao).map(DadosListagemMedico::new);
+	}
+
+	/*
+	@GetMapping
+	public List<DadosListagemMedico> listar(){
+		return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+	}
+	*/
 }
